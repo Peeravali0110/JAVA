13b1e69118701c12316903d98c41c7562bf5a948
peeravali0110 <peeravali0110@gmail.com>
2022-12-22 11:06:34 +0530

    java

---
+++ practice/seven.java
@@ -0,0 +1,18 @@
+class Test{
+ 
+ public static void main(String[] args)
+ {
+ 
+ int[] a=new int[4];
+ String[] b = new  String[3];
+ 
+System.out.println(a[0]);
+System.out.println(b[0]);
+ 
+ 
+ }
+
+
+
+
+}
