d5fd5c01156eef4c9f74e3dd0a3fcb944d055ffe
peeravali0110 <peeravali0110@gmail.com>
2022-12-24 21:17:27 +0530

    okay

---
+++ practice2/Test16.java
@@ -0,0 +1,20 @@
+class Test {
+
+public static void main(String[] args)
+{
+
+String[] names={"Aryan","Peer vali","azma","ayyat","syed"};
+
+int i=0;
+while(i<=names.length-1){
+	
+	System.out.println(names[i]);
+	i=i+1;
+
+}
+
+
+}
+
+
+}
--- practice2/table.java
+++ practice2/table.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 int i=1;
 while(i<=10){
 
-System.out.println("6*" +i + "=" +6*i);
+System.out.println("6*" + i + "=" +6*i);
 
 i=i+1;
 
