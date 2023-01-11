640dce096f5d7af6fcbb6c039f62c4ff63544ff1
peeravali0110 <peeravali0110@gmail.com>
2023-01-11 21:06:44 +0530

    wed

---
+++ practice6/Table.java
@@ -0,0 +1,33 @@
+// wap to print 5 table
+
+
+class Table{
+	
+	
+public static void main(String[] args){
+	
+	for(int i=5;i<=50;i=i+5){
+		
+		System.out.println(i);
+		
+		
+		
+}
+
+ 
+ //5*1=5
+ 
+ 
+ 
+for(int i=1;i<=10;i=i+1){  
+	
+	System.out.println("5*" +i+ "=" +5*i);
+	
+}
+
+
+	
+}
+}
+
+
---
+++ practice6/parent.java
@@ -0,0 +1,38 @@
+class Parent{
+
+public void m1(){
+
+System.out.println("aryan");
+
+
+}	
+	
+	
+	
+	
+}
+class Child extends Parent{
+	
+	public void m1(){
+		
+		System.out.println("hello gm");
+		
+	}
+	
+	
+	
+	
+	
+}
+class Test {
+	
+public static void main(String[] args){
+
+Child C1=new Child();
+C1.m1();
+//C1.m1();
+
+
+}	
+	
+}
